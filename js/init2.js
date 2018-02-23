5e80db7eea9bdc947d02cfe727d5276844e8bbe3
cyrilphoenix71 <cyrilphoenix71@gmail.com>
2018-02-23 10:50:45 +0100

    Update init2.js

--- js/init2.js
+++ js/init2.js
@@ -80,7 +80,7 @@ function generatePlugins(_data){
 						<a class="waves-effect waves-light btn" href=https://jeedom-plugins-extra.github.io/plugin-'+_data.third_plugin.docs[i].name+'/fr_FR>Doc</a>\
 						<a class="waves-effect waves-light btn" href=https://github.com/Jeedom-Plugins-Extra/plugin-'+_data.third_plugin.docs[i].name+'/issues>Issues</a>\
 						<a class="waves-effect waves-light btn" href=https://jeedom-plugins-extra.github.io/plugin-'+_data.third_plugin.docs[i].name+'/fr_FR/changelog>Changelog</a>\
-						<a href=https://waffle.io/Jeedom-Plugins-Extra/'+_data.third_plugin.docs[i].description+'><img src=https://badge.waffle.io/Jeedom-Plugins-Extra/plugin-'+_data.third_plugin.docs[i].name+'.svg?columns=all class="center"></a>\
+						<a href=https://waffle.io/Jeedom-Plugins-Extra/'+_data.third_plugin.docs[i].name+'><img src=https://badge.waffle.io/Jeedom-Plugins-Extra/plugin-'+_data.third_plugin.docs[i].name+'.svg?columns=all class="center"></a>\
 					</div>\
 				</div>\
 			</div>\
