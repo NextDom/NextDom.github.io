d0f2569f61173364fe722901f111be2e51fe721d
byackee <vincent.fresnel@gmail.com>
2018-02-27 14:43:18 +0100

    update

--- js/init2.js
+++ js/init2.js
@@ -19,14 +19,14 @@ $(function(){
 		setTimeout(function(){ $('#wiki a.active').click(); }, 500);
 	});
 
-	$('a[href=#developer]').on('click',function(){
+	$('a[href=#developpement]').on('click',function(){
 		$('#developer a.active').click();
 		setTimeout(function(){ $('#developer a.active').click(); }, 500);
 	});
 
 	use='offline';
 	if (use=='online'){
-		generateRepos("https://api.github.com/orgs/jeedom-plugins-extra/repos");
+		generateRepos("https://api.github.com/orgs/Jeedom-Plugins-Extra/repos?per_page=100");
 		generateTeam("json/members.json");
 		generateWiki("json/wiki.json");
 		generateChat("json/chat.json");
@@ -47,26 +47,26 @@ function convertCase(_string){
 }
 
 function generateRepos(_json){
-var nbrissues =0;
+var nbrissues = 0;
 var nbrrepos = 0;
 	requestJSON(_json, function(data) {
 		$.each(data, function(i) {
 			if (data[i].name !='Jeedom-Plugins-Extra.github.io' && data[i].name !='Jeedom-Plugins-Extra' && data[i].name !='custom-jeedom'){
 				$('#ul_listPluginThird').append('\
                 	<div class="col s8 m4">\
-        	        	<div class="card small hoverable sticky-action">\
-                        	<span class="card-title center-align">'+data[i].name.substr(7)+'</a></span>\
+        	        	<div class="card medium hoverable sticky-action">\
+                        	<span class="card-title center-align"><img src="'+data[i].html_url+'/blob/master/plugin_info/'+data[i].name.substr(7)+'_icon.png?raw=true" width="50%" height="50%" class="center">\</a></span>\
 		                    <div class="card-text">\
-		                    	<img src="'+data[i].html_url+'/blob/master/plugin_info/'+data[i].name.substr(7)+'_icon.png?raw=true" width="25%" height="25%" class="center">\
-								<p>'+data[i].description+'</p>\
+		                    	'+data[i].description+'\
 		                    </div>\
                         	<div class="card-action center-align">\
+                        	    <a href=https://github.com/Jeedom-Plugins-Extra/'+data[i].name+'/issues><img src=https://img.shields.io/github/issues/Jeedom-Plugins-Extra/'+data[i].name+'.svg?longCache=true&style=flat-square></a>\
+		                    	 <img src=https://img.shields.io/github/stars/Jeedom-Plugins-Extra/'+data[i].name+'.svg?longCache=true&style=flat-square><br>\
                             	<a class = "btn-floating waves-effect waves-light blue-grey"  href='+data[i].owner.html_url+'/'+data[i].owner.login+'/wiki/03-Liste-Plugins-&-Roadmap#'+data[i].name+'><i class ="fas fa-info"></i></a>\
-                            	<a class = "btn-floating waves-effect waves-light orange" href=https://github.com/Jeedom-Plugins-Extra/'+data[i].name+'/issues><i class="fas fa-bug"></i></a>\
+                            	<a class = "btn-floating waves-effect waves-light bg-orange" href=https://github.com/Jeedom-Plugins-Extra/'+data[i].name+'/issues><i class="fas fa-bug"></i></a>\
 								<a class = "btn-floating waves-effect waves-light light-green"  href=https://jeedom-plugins-extra.github.io/'+data[i].name+'/fr_FR><i class = "fas fa-book"></i></a>\
 	                        	<a class = "btn-floating waves-effect waves-light grey"  href=https://jeedom-plugins-extra.github.io/'+data[i].name+'/fr_FR/changelog><i class ="fas fa-history"></i></a>\
-								<a class="btn-floating btn-small red right">'+data[i].open_issues+'</a>\
-							</div>\
+    		                   </div>\
                         </div>\
                 </div>');
 			nbrissues = nbrissues + data[i].open_issues;
@@ -85,10 +85,14 @@ function generateTeam(_json){
             	<div class="col m2">\
 	        		<div class="card hoverable sticky-action">\
                     	<span class="card-title center-align">'+data[i].login+'</span>\
-	                    <div class="card-text">\
+	                    <div class="card-text ">\
 	                    	<a href='+data[i].html_url+'><img src="'+data[i].avatar_url+'" width="90" height="90" class="center"></a>\
 					    <br>\
 	                   	</div>\
+	                   		<div class="card-action center-align">\
+	                   		    <a class = "btn-floating waves-effect waves-light blue-grey"  href=https://github.com/Jeedom-Plugins-Extra/Jeedom-Plugins-Extra/wiki/02---Pr%C3%A9sentation-des-membres-de-la-Team#'+data[i].login+'><i class ="fas fa-user"></i></a>\
+                                <a class = "btn-floating waves-effect waves-light light-green" href=https://paypal.me/'+data[i].login+'><i class="fas fa-beer"></i></a>\
+	                   		</div>\
                     </div>\
                 </div>');
 			nbrmembres = nbrmembres + 1;
@@ -108,7 +112,16 @@ function generateWiki(_json){
 			return 0;
 			});
 		for(var i in data.wiki.docs){
-			$('#ul_listWiki').append('<div class="col s4 m4"><div class="card horizontal hoverable sticky-action"><div class="card-text"><p><a href="'+data.wiki.docs[i].url+'" style="color:black; text-align:center">'+data.wiki.docs[i].name+'</a></p></div></div></div></div>');
+			$('#ul_listWiki').append('\
+			    <div class="col s6 m6">\
+			        <div class="card hoverable">\
+			            <div class="card-text">\
+			            <br>\
+			                <a class="center" href="'+data.wiki.docs[i].url+'" style="color:black; text-align:center">'+data.wiki.docs[i].name+'</a>\
+		                <br>\
+		                </div>\
+	                </div>\
+                </div>');
 		}
 	});
 }
@@ -119,10 +132,12 @@ function generateChat(_json){
 
 	for(var i in data.chat.docs){
 		$('#ul_listChat').append('\
-			<div class="col s4 m4">\
-				<div class="card horizontal hoverable sticky-action">\
+			<div class="col s6 m6">\
+				<div class="card hoverable">\
 					<div class="card-text">\
-						<p><a href="'+data.chat.docs[i].url+'" style="color:black; text-align:center; display:block">'+data.chat.docs[i].name+'</a></p>\
+					<br>\
+						<a class="center" href="'+data.chat.docs[i].url+'" style="color:black; text-align:center">'+data.chat.docs[i].name+'</a>\
+						<br>\
 						</div>\
 					</div>\
 				</div>\
@@ -143,10 +158,12 @@ function generateDeveloper(_json){
 		});
 		for(var i in data.developer.docs){
 			$('#ul_listDeveloper').append('\
-				<div class="col s4 m4">\
-					<div class="card horizontal hoverable sticky-action">\
+				<div class="col s6 m6">\
+					<div class="card hoverable">\
 						<div class="card-text">\
-							<p><a href="'+data.developer.docs[i].url+'" style="color:black; text-align:center; display:block">'+data.developer.docs[i].name+'</a></p>\
+						<br>\
+							<a class="center" href="'+data.developer.docs[i].url+'" style="color:black; text-align:center; display:block">'+data.developer.docs[i].name+'</a>\
+							<br>\
 							</div>\
 						</div>\
 					</div>\
